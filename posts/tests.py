49410c8ca6fe4bd1dbf204a4f14688e98c6dffcf
MehediMashfan086 <mehedihasan62086@gmail.com>
2022-04-18 13:54:41 +0600

    delete_api

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,8 @@
+import pytest
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+def test_should_create_user_with_username(db) -> None:
+    user = User.objects.create_user("admin")
+    assert user.username == "admin"
